1a9b93e09ee1220454100de6636bfc55d126e339
Ezequiel Cabello <cabello.marcos.ezequiel@gmail.com>
2023-10-19 21:27:51 -0300

    Starting the styling of the results page

--- app/result/page.jsx
+++ app/result/page.jsx
@@ -1,3 +1,38 @@
-export default function Result() {
-    return <h1>Result Page</h1>
-}
+const Page = () => {
+    return (
+      <div className="text-fontWhite text-4xl">
+        <p className="text-2xl font-bold">
+          Awesome!
+        </p>
+        <p className="text-2xl font-bold">
+            Copy AI made  this copies for you 😎
+        </p>
+  
+        <div className="w-[390px] h-[237px] left-4 top-[272px] absolute">
+          <div className="w-[170px] h-[206px] left-0 top-8 absolute">
+            <div className="w-full h-full bg-zinc-500 bg-opacity-30 rounded-lg"></div>
+            <p className="w-[153px] h-[172px] left-4 top-4 absolute text-gray-200 text-base font-normal font-dm-sans">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <div className="w-[28.45px] h-[28.45px] left-[131px] top-[169px] absolute"></div>
+          </div>
+          <div className="w-[170px] h-[206px] left-[176px] top-8 absolute">
+            <div className="w-full h-full bg-zinc-500 bg-opacity-30 rounded-lg"></div>
+            <p className="w-[153px] h-[172px] left-4 top-4 absolute text-gray-200 text-base font-normal font-dm-sans">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <div className="w-[28.45px] h-[28.45px] left-[131px] top-[169px] absolute"></div>
+          </div>
+          <p className="w-[94px] h-[31px] left-1 top-0 absolute text-white text-base font-bold font-dm-sans">
+            Copy A
+          </p>
+          <p className="w-[94px] h-[31px] left-[185px] top-0 absolute text-white text-base font-bold font-dm-sans">
+            Copy B
+          </p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Page;
+  
