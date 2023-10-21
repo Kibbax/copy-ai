8b0f587ee9adfbc276159ff06ec64309284f7cb4
Ezequiel Cabello <cabello.marcos.ezequiel@gmail.com>
2023-10-21 17:48:09 -0300

    styling result pages & bg

--- app/result/page.jsx
+++ app/result/page.jsx
@@ -1,11 +1,12 @@
 const Page = () => {
   return (
-    <div>
+    <div className="absolute z-10">
+      
       <div className="container">
         <h1 className="text-fontPurple text-4xl">AWESOME!</h1>
         <p className="text-fontPurple text-2xl">Check out your results</p>
 
-        <div  className="flex px-4 py-4 text-fontWhite justify-around">
+        <div className="flex px-4 py-4 text-fontWhite justify-around">
           <div className="mx-2 px-4 bg-inputColor bg-opacity-10 rounded-lg">
             <p>Copy A</p>
             <p>
@@ -14,8 +15,9 @@ const Page = () => {
               explicabo!.
             </p>
             <div>
-              <button className="mt-6 align-center px-4 py-2 bg-buttonBlack rounded-lg">Copy</button> 
-              
+              <button className="mt-6 align-center px-4 py-2 bg-buttonBlack rounded-lg">
+                Copy
+              </button>
             </div>
           </div>
 
@@ -27,14 +29,17 @@ const Page = () => {
               explicabo!.
             </p>
             <div>
-            <button className="mt-6 align-center px-4 py-2 bg-buttonBlack rounded-lg">Copy</button>
+              <button className="mt-6 align-center px-4 py-2 bg-buttonBlack rounded-lg">
+                Copy
+              </button>
             </div>
           </div>
-
         </div>
       </div>
       <div>
-        <button className="mt-6 align-center px-4 py-2 bg-buttonBlack rounded-lg text-fontWhite">Download PDF</button>
+        <button className="mt-6 align-center px-4 py-2 bg-buttonBlack rounded-lg text-fontWhite">
+          Download PDF
+        </button>
       </div>
     </div>
   );
