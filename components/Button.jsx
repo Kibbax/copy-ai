514aad8043be9bfd903469252b8b84b05ae97e64
María Macarena Alvarez Castillo <mariamacarenaac@gmail.com>
2023-10-22 18:55:15 -0300

    details on button

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ text}) => {
   return (
     <button
-    className="relative z-10 w-1/2 h-10 flex-shrink-0 rounded-md bg-opacity-33 bg-buttonBlack hover:bg-hoverColor text-fontWhite font-bold"
+    className="w-1/2 h-10 flex-shrink-0 rounded-md bg-opacity-33 bg-buttonBlack hover:bg-hoverColor text-fontWhite font-bold"
     >
 
       {text}
