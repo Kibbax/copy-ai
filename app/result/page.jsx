824571a3c1dba0b26cd47275bc20ffa08a7d785a
Ezequiel Cabello <cabello.marcos.ezequiel@gmail.com>
2023-10-23 11:57:42 -0300

    Refined component view styles

--- app/result/page.jsx
+++ app/result/page.jsx
@@ -1,45 +1,40 @@
+import Button from "@/components/Button";
+import Title from "@/components/Title";
+
 const Page = () => {
   return (
-    <div className="flex justify-center pt-36 md:w-1/2 lg:w-1/3 m-auto">
-      
-      <div className="container">
-        <h1 className="text-fontPurple text-4xl font-titleFont">awesome!</h1>
-        <p className="text-fontPurple text-2xl font-textFont">Check out your results</p>
+    <div className="w-3/4 md:w-1/2 lg:w-1/3 m-auto pt-20 pb-10 text-gray-200">
+      <Title text="awesome!!" />
+      <h2 className="text-lg">Copy AI crafted these captivating copies just for you! 😎</h2>
 
-        <div className="flex px-4 py-4 text-fontWhite justify-around">
-          <div className="mx-2 px-4 bg-inputColor bg-opacity-10 rounded-lg">
-            <p>Copy A</p>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Doloremque praesentium porro quo dignissimos, iure tempore
-              explicabo!.
-            </p>
-            <div>
-              <button className="mt-6 align-center px-4 py-2 bg-buttonBlack rounded-lg">
-                Copy
-              </button>
-            </div>
+      <div className="flex px-4 py-4 text-gray-200 text-sm font-regular justify-around">
+        <div className="mx-2 px-4 bg-zinc-500 bg-opacity-30 rounded-lg">
+          <p>Copy A</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque praesentium porro quo dignissimos, iure tempore explicabo!.
+          </p>
+          <div>
+            <button className="mt-6 justify-end px-4 py-2 bg-buttonBlack rounded-lg text-fontWhite">
+              Copy
+            </button>
           </div>
+        </div>
 
-          <div className="mx-2 px-4 bg-inputColor bg-opacity-10 rounded-lg">
-            <p>Copy B</p>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Doloremque praesentium porro quo dignissimos, iure tempore
-              explicabo!.
-            </p>
-            <div>
-              <button className="mt-6 align-center px-4 py-2 bg-buttonBlack rounded-lg">
-                Copy
-              </button>
-            </div>
+        <div className="mx-2 px-4 bg-zinc-500 bg-opacity-30 rounded-lg">
+          <p>Copy B</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque praesentium porro quo dignissimos, iure tempore explicabo!.
+          </p>
+          <div>
+            <button className="mt-6 align-center px-4 py-2 bg-buttonBlack rounded-lg text-fontWhite">
+              Copy
+            </button>
           </div>
         </div>
       </div>
-      <div>
-        <button className="mt-6 align-center px-4 py-2 bg-buttonBlack rounded-lg text-fontWhite">
-          Download PDF
-        </button>
+
+      <div className="text-center align-baseline">
+        <Button text="Download PDF" />
       </div>
     </div>
   );
