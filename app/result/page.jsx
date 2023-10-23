e2656d422c0dab7f52f309acc7409d82b81f9f27
Ezequiel Cabello <cabello.marcos.ezequiel@gmail.com>
2023-10-23 09:55:25 -0300

    uploaded fonts

--- app/result/page.jsx
+++ app/result/page.jsx
@@ -1,10 +1,10 @@
 const Page = () => {
   return (
-    <div className="absolute z-10">
+    <div className="flex justify-center pt-36 md:w-1/2 lg:w-1/3 m-auto">
       
       <div className="container">
-        <h1 className="text-fontPurple text-4xl">AWESOME!</h1>
-        <p className="text-fontPurple text-2xl">Check out your results</p>
+        <h1 className="text-fontPurple text-4xl font-titleFont">awesome!</h1>
+        <p className="text-fontPurple text-2xl font-textFont">Check out your results</p>
 
         <div className="flex px-4 py-4 text-fontWhite justify-around">
           <div className="mx-2 px-4 bg-inputColor bg-opacity-10 rounded-lg">
