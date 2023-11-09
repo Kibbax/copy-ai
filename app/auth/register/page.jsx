9d9eb83ef52eda580c9d98411708c445961ee46e
María Macarena Alvarez Castillo <mariamacarenaac@gmail.com>
2023-11-09 16:07:51 -0300

    I added validations on the length of the username

--- app/auth/register/page.jsx
+++ app/auth/register/page.jsx
@@ -2,38 +2,38 @@
 import Button from "@/components/Button";
 import Title from "@/components/Title";
 import { useForm } from "react-hook-form";
-import {useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function Register() {
-
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const router = useRouter();
-  
 
-  const onSubmit = handleSubmit(async data => {
-
-    if(data.password !== data.confirmPassword){
-      return alert("Passwords do not match")
+  const onSubmit = handleSubmit(async (data) => {
+    if (data.password !== data.confirmPassword) {
+      return alert("Passwords do not match");
     }
 
-    const res = await fetch('/api/auth/signup', {
-      method: 'POST',
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         name: data.username,
         email: data.email,
-        password: data.password
-      })
-    })
-  /* const resJSON = await res.json(); */ // quitar al tener la ruta de longing
-  //activar cuando se tenga la ruta loging
-  if(res.ok){
-    router.push('/auth/login')
-  }
-  
-  })
+        password: data.password,
+      }),
+    });
+    /* const resJSON = await res.json(); */ // quitar al tener la ruta de longing
+    //activar cuando se tenga la ruta loging
+    if (res.ok) {
+      router.push("/auth/login");
+    }
+  });
 
   return (
     <div className="text-gray-200">
@@ -46,29 +46,33 @@ export default function Register() {
           <label
             htmlFor="username"
             className="block text-gray-700 text-sm font-bold mb-2"
-          >
-          </label>
+          ></label>
           <input
             type="text"
             id="name"
-            
             placeholder="Name"
             className="w-full py-2 px-3 bg-zinc-500 bg-opacity-30 rounded-lg focus:outline-none"
-            {...register('username', { 
+            {...register("username", {
               required: {
                 value: true,
-                message: "Please enter your name"
-              } })}
+                message: "Please enter your name",
+              },
+              maxLength: 20,
+              minLength: 2,
+            })}
           />
           {errors.username && (
-            <span className="text-red-500">
-              {errors.username.message} 
-            </span>
+            <span className="text-red-500">{errors.username.message}</span>
           )}
-
+          {errors.username?.type === "maxLength" && (
+            <span>Name must not be longer than 20 characters</span>
+          )}
+          {errors.username?.type === "minLength" && (
+            <span>Name must be greater than 2 characters</span>
+          )}
         </div>
 
-      {/*   <div className="mb-4">
+        {/*   <div className="mb-4">
           <label
             htmlFor="surname"
             className="block text-gray-700 text-sm font-bold mb-2"
@@ -92,19 +96,17 @@ export default function Register() {
           <input
             type="email"
             id="email"
-            
             placeholder="E-mail adress"
             className="w-full py-2 px-3  bg-zinc-500 bg-opacity-30 rounded-lg focus:outline-none"
-            {...register('email', { 
+            {...register("email", {
               required: {
                 value: true,
-                message: "Please enter your e-mail"
-              } })}
+                message: "Please enter your e-mail",
+              },
+            })}
           />
           {errors.email && (
-            <span className="text-red-500">
-              {errors.email.message} 
-            </span>
+            <span className="text-red-500">{errors.email.message}</span>
           )}
         </div>
 
@@ -132,38 +134,35 @@ export default function Register() {
           <input
             type="password"
             id="password"
-            
             placeholder="Password"
             className="w-full py-2 px-3 bg-zinc-500 bg-opacity-30 rounded-lg focus:outline-none"
-            {...register('password', { 
+            {...register("password", {
               required: {
                 value: true,
-                message: "Please enter your password"
-              } })}
+                message: "Please enter your password",
+              },
+            })}
           />
           {errors.password && (
-            <span className="text-red-500">
-              {errors.password.message}
-            </span>
+            <span className="text-red-500">{errors.password.message}</span>
           )}
         </div>
         <div className="mb-4">
           <label
             htmlFor="confirmPassword"
             className="block text-gray-700 text-sm font-bold mb-2"
-          >
-          </label>
+          ></label>
           <input
             type="password"
             id="confirmPassword"
-            
             placeholder="Confirm Password"
             className="w-full py-2 px-3 bg-zinc-500 bg-opacity-30 rounded-lg focus:outline-none"
-            {...register('confirmPassword', { 
+            {...register("confirmPassword", {
               required: {
                 value: true,
-                message: "Please confirm your password"
-              } })}
+                message: "Please confirm your password",
+              },
+            })}
           />
           {errors.confirmPassword && (
             <span className="text-red-500">
