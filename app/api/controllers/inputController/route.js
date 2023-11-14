7e459238c18af3e6a8bc23f12139893c7bae88e9
Kibbax <cabello.marcos.ezequiel@gmail.com>
2023-11-14 05:51:06 -0300

    recoding input to backend

--- app/api/controllers/inputController/route.js
+++ app/api/controllers/inputController/route.js
@@ -1,47 +1,33 @@
-// /api/controllers/inputController.js
+import db from '@/lib/db'
+import { authOptions } from '../../auth/[...nextauth]/route'
+import { getServerSession } from 'next-auth'
 
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-      await handlePost(req, res);
-    } else {
-      res.status(405).end(); // Método no permitido para otras solicitudes
-    }
+
+
+export const POST = async (req, res) => {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    return res.status(401).json({ error: 'Unauthorized' });
   }
-  
-  async function handlePost(req, res) {
-    try {
-      const session = await getServerSession(authOptions);
-  
-      if (!session) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-  
-      const { prompt, value, targetAge } = req.body;
-  
-      // Verifica que los datos necesarios estén presentes en la solicitud
-      if (!prompt) {
-        return res.status(400).json({ error: 'Missing required parameter: prompt.' });
-      }
-  
-      // Obtiene el userId del contexto de autenticación
-      const userId = session.user.id;
-  
-      // Lógica para guardar en la base de datos o cualquier otra acción necesaria
-      const newInput = await db.content_input.create({
-        data: {
-          userId: userId,
-          content: prompt,
-          // Trata con valores opcionales (asegúrate de tener "value" y "targetAge" definidos)
-          networkType: value || null,
-          targetAge: targetAge || null,
-        },
-      });
-  
-      // Devuelve una respuesta al cliente indicando que la operación fue exitosa
-      return res.status(201).json({ message: 'Input created successfully.', data: newInput });
-    } catch (error) {
-      console.error('Error handling POST request:', error);
-      return res.status(500).json({ error: 'Internal server error.', detailedError: error.message });
-    }
+
+  const { prompt, value, targetAge } = req.body;
+
+  if (!prompt) {
+    return res.status(400).json({ error: 'Missing required parameter: prompt.' });
   }
-  
+
+  const userId = session.user.id;
+
+  const newInput = await db.content_input.create({
+    data: {
+      userId: userId,
+      content: prompt,
+      networkType: value,
+      targetAge: targetAge,
+    }
+  });
+  console.log(newInput)
+  return newInput;
+
+}
--- app/input/page.jsx
+++ app/input/page.jsx
@@ -1,83 +1,60 @@
-"use client";
-import { useState } from "react";
+"use client"
 import Button from "@/components/Button";
 import Title from "@/components/Title";
+import { useState } from "react";
 
 export default function InputPage() {
-  const [input, setInput] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [successMessage, setSuccessMessage] = useState(null);
+    const [input, setInput] = useState("")
 
-  const handleChange = (e) => {
-    setInput(e.target.value);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    setIsLoading(true);
-    setErrorMessage(null);
-    setSuccessMessage(null);
-
-    try {
-      console.log('Sending request with data:', { prompt: input, value: 'Instagram', targetAge: 25 });
-
-      const response = await fetch('/api/controllers/inputController', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          prompt: input,
-          value: 'Instagram',
-          targetAge: 25,
-        }),
-      });
-
-      console.log('Response status:', response.status);
-
-      if (!response.ok) {
-        console.error('Failed to submit data. Response status:', response.status);
-        throw new Error('Failed to submit data');
-      }
-
-      const data = await response.json();
-      setSuccessMessage(data.message);
-    } catch (error) {
-      console.error('Error:', error);
-      setErrorMessage('Failed to submit data. Please try again.');
-    } finally {
-      setIsLoading(false);
+    const handleChange = (e)=>{
+        setInput(e.target.value)
+        console.log(e.target.value)
     }
-  };
+    
+  
 
-  return (
-    <div className="flex justify-center pt-36 md:w-1/2 lg:w-1/3 m-auto">
-      <div className="flex flex-col w-5/6 h-[370px] items-center justify-between">
-        <div className="w-full">
-          <Title text={"Create your plan"} />
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+
+    fetch('/api/result', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json', 
+          },
+        body: JSON.stringify({prompt:input})
+    })
+    /* const data = await inputSend.json()
+    return data */
+    .then(response => response.json()) 
+    .then(data => {
+      console.log(data); 
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+
+  }
+    
+    return  <div className="flex justify-center pt-36 md:w-1/2 lg:w-1/3 m-auto">
+        <div className="flex flex-col w-5/6 h-[370px] items-center justify-between">
+            <div className="w-full">
+            <Title text={"Create your plan"}/>
+            </div>
+            <form className="flex flex-col  justify-center w-full h-[320px]" onSubmit={handleSubmit}>
+                <label className="text-fontWhite text-xl mb-5" htmlFor="ideaInput">Input your idea:</label>
+                <textarea
+                id="ideaInput"
+                className="w-full h-[159px] bg-opacity-30 text-fontWhite rounded-md bg-inputColor p-5 focus:outline-none focus:ring focus:ring-linesColors"
+                type="text"
+                placeholder="Enter your idea"
+                onChange={handleChange}
+                required
+                ></textarea>
+                <div className="m-auto text-center">
+                <Button text={"Submit"} />
+                </div>
+            </form>
+            
         </div>
-        <form className="flex flex-col justify-center w-full h-[320px]" onSubmit={handleSubmit}>
-          <label className="text-fontWhite text-xl mb-5" htmlFor="ideaInput">
-            Input your idea:
-          </label>
-          <textarea
-            id="ideaInput"
-            className="w-full h-[159px] bg-opacity-30 text-fontWhite rounded-md bg-inputColor p-5 focus:outline-none focus:ring focus:ring-linesColors"
-            type="text"
-            placeholder="Enter your idea"
-            onChange={handleChange}
-            required
-          ></textarea>
-          <div className="m-auto text-center">
-            <Button text={"Submit"} disabled={isLoading} />
-            {isLoading && <p>Loading...</p>}
-            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-            {successMessage && <p className="text-green-500">{successMessage}</p>}
-          </div>
-        </form>
-      </div>
     </div>
-  );
 }
