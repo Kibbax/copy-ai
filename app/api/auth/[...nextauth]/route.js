6676a978cc1e92ca49ca34bd5e4823010845ad3d
Ezequiel Cabello <cabello.marcos.ezequiel@gmail.com>
2023-11-14 01:07:49 -0300

    creating conexion frontend to backend

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,7 +3,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import db from '@/lib/db' 
 import bcrypt from 'bcrypt'
-import Register from "@/app/auth/registerGoogle/route";
+
 
 export const authOptions = {
   providers: [
--- app/input/page.jsx
+++ app/input/page.jsx
@@ -1,60 +1,83 @@
-"use client"
+"use client";
+import { useState } from "react";
 import Button from "@/components/Button";
 import Title from "@/components/Title";
-import { useState } from "react";
 
 export default function InputPage() {
-    const [input, setInput] = useState("")
+  const [input, setInput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
 
-    const handleChange = (e)=>{
-        setInput(e.target.value)
-        console.log(e.target.value)
-    }
-    
-  
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
-  const handleSubmit = async (e)=>{
-    e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    fetch('/api/result', {
-        method: "POST",
+    setIsLoading(true);
+    setErrorMessage(null);
+    setSuccessMessage(null);
+
+    try {
+      console.log('Sending request with data:', { prompt: input, value: 'Instagram', targetAge: 25 });
+
+      const response = await fetch('/api/controllers/inputController', {
+        method: 'POST',
         headers: {
-            'Content-Type': 'application/json', 
-          },
-        body: JSON.stringify({prompt:input})
-    })
-    /* const data = await inputSend.json()
-    return data */
-    .then(response => response.json()) 
-    .then(data => {
-      console.log(data); 
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          prompt: input,
+          value: 'Instagram',
+          targetAge: 25,
+        }),
+      });
 
-  }
-    
-    return  <div className="flex justify-center pt-36 md:w-1/2 lg:w-1/3 m-auto">
-        <div className="flex flex-col w-5/6 h-[370px] items-center justify-between">
-            <div className="w-full">
-            <Title text={"Create your plan"}/>
-            </div>
-            <form className="flex flex-col  justify-center w-full h-[320px]" onSubmit={handleSubmit}>
-                <label className="text-fontWhite text-xl mb-5" htmlFor="ideaInput">Input your idea:</label>
-                <textarea
-                id="ideaInput"
-                className="w-full h-[159px] bg-opacity-30 text-fontWhite rounded-md bg-inputColor p-5 focus:outline-none focus:ring focus:ring-linesColors"
-                type="text"
-                placeholder="Enter your idea"
-                onChange={handleChange}
-                required
-                ></textarea>
-                <div className="m-auto text-center">
-                <Button text={"Submit"} />
-                </div>
-            </form>
-            
+      console.log('Response status:', response.status);
+
+      if (!response.ok) {
+        console.error('Failed to submit data. Response status:', response.status);
+        throw new Error('Failed to submit data');
+      }
+
+      const data = await response.json();
+      setSuccessMessage(data.message);
+    } catch (error) {
+      console.error('Error:', error);
+      setErrorMessage('Failed to submit data. Please try again.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex justify-center pt-36 md:w-1/2 lg:w-1/3 m-auto">
+      <div className="flex flex-col w-5/6 h-[370px] items-center justify-between">
+        <div className="w-full">
+          <Title text={"Create your plan"} />
         </div>
+        <form className="flex flex-col justify-center w-full h-[320px]" onSubmit={handleSubmit}>
+          <label className="text-fontWhite text-xl mb-5" htmlFor="ideaInput">
+            Input your idea:
+          </label>
+          <textarea
+            id="ideaInput"
+            className="w-full h-[159px] bg-opacity-30 text-fontWhite rounded-md bg-inputColor p-5 focus:outline-none focus:ring focus:ring-linesColors"
+            type="text"
+            placeholder="Enter your idea"
+            onChange={handleChange}
+            required
+          ></textarea>
+          <div className="m-auto text-center">
+            <Button text={"Submit"} disabled={isLoading} />
+            {isLoading && <p>Loading...</p>}
+            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+            {successMessage && <p className="text-green-500">{successMessage}</p>}
+          </div>
+        </form>
+      </div>
     </div>
+  );
 }
