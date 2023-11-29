089772fb485feda5c9166ab3137181d0eb8f3936
Ezequiel Cabello <cabello.marcos.ezequiel@gmail.com>
2023-11-29 14:51:47 -0300

    Refactor logo component and update background styling

--- components/Background.jsx
+++ components/Background.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React from "react";
 
 const Background = () => {
   return (
     <>
-      {/* Círculo con borde */}
-      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 skew-x-[-60deg] w-[576px] h-[576px] bg-transparent border-2 border-violet-900 border-opacity-30 rounded-full rotate-[-45deg] -z-10" />
+      <div className="-z-10">
+        {/* Logo */}
+        <div className="fixed grid grid-cols-3 grid-rows-3 gap-2 w-[130px] m-auto mt-20 opacity-30 left-[40rem]">
+          {/* Rectángulo 1 */}
+          <div className="bg-violet-800 w-[80px] h-[40px] rounded-bl-full rounded-tr-full col-span-2 row-span-1"></div>
 
+          {/* Cuadrado 2 */}
+          <div className="bg-violet-800 w-[40px] h-[40px] rounded-tr-full rounded-tl-full rounded-bl-full col-span-1 overflow-hidden">
+            <img
+              src="/assets/celu-en-mano.webp"
+              alt="Image"
+              className="w-full h-full object-cover"
+            />
+          </div>
+
+          {/* Cuadrado 3 */}
+          <div className="bg-violet-800 w-[40px] h-[40px]  rounded-tr-full rounded-br-full rounded-bl-full col-span-1 overflow-hidden">
+            <img
+              src="/assets/trabajando.webp"
+              alt="Image"
+              className="w-full h-full object-cover"
+            />
+          </div>
+
+          {/* Cuadrado 4 */}
+          <div className="bg-violet-800 w-[40px] h-[40px] col-span-1"></div>
+
+          {/* Cuadrado 5 */}
+          <div className="bg-violet-800 w-[40px] h-[40px]  rounded-bl-full col-span-1 overflow-hidden">
+            <img
+              src="/assets/notebook.webp"
+              alt="Image"
+              className="w-full h-full object-cover"
+            />
+          </div>
+
+          {/* Cuadrado 6 */}
+          <div className="bg-violet-800 w-[40px] h-[40px] rounded-full col-span-1"></div>
+
+          {/* Rectángulo 7 */}
+          <div className="bg-violet-800 w-[80px] h-[40px] rounded-tl-full rounded-bl-full rounded-tr-full col-span-2 row-span-1 overflow-hidden">
+            <img
+              src="/assets/muchos-celus.webp"
+              alt="Image"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </div>
+        {/* Círculo con borde */}
+        <div className="fixed top-[52rem] left-1/4 transform -translate-x-1/2 -translate-y-1/2 skew-x-[-50deg] w-[576px] h-[576px] bg-transparent border-2 border-violet-900 border-opacity-30 rounded-full rotate-[136deg] -z-10" />
+      </div>
     </>
   );
 };
