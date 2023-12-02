a29ed0137017d5adb751d56964c40cdc2d5e011d
Ezequiel Cabello <cabello.marcos.ezequiel@gmail.com>
2023-12-02 19:35:37 -0300

    Refactor DocPDF component styles

--- components/DocPDF.jsx
+++ components/DocPDF.jsx
@@ -1,93 +1,75 @@
-import React from 'react';
-import { Document, Page, Text, View, Image, StyleSheet } from '@react-pdf/renderer';
+  import React from 'react';
+  import { Document, Page, Text, View, Link, StyleSheet } from '@react-pdf/renderer';
 
-const styles = StyleSheet.create({
-  page: {
-    flexDirection: 'column',
-    margin: 20,
-    marginBottom: 40, // Ajusta el margen inferior según sea necesario
-    
-  },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  logo: {
-    width: 40,
-    height: 40,
-    marginRight: 10,
-  },
-  headerText: {
-    fontSize: 12,
-    fontWeight: 'bold',
-  },
-  title: {
-    fontSize: 18,
-    textAlign: 'center',
-    marginBottom: 10,
-  },
-  section: {
-    marginBottom: 10,
-  },
-  inputSection: {
-    textAlign: 'right',
-  },
-  resultSection: {
-    textAlign: 'left',
-    marginTop: 20,
-  },
-  separator: {
-    borderBottom: 1,
-    borderBottomColor: '#240046',
-    marginBottom: 10,
-  },
-  footer: {
-    position: 'absolute',
-    bottom: 30,
-    left: 20,
-    right: 20,
-    borderTop: 1,
-    borderTopColor: '#240046',
-    paddingTop: 5,
-    textAlign: 'center',
-    fontSize: 10,
-  },
-});
+  const styles = StyleSheet.create({
+    page: {
+      flexDirection: "column",
+      backgroundColor: "#F0DCF0",
+      padding: 20,
+    },
+    title: {
+      fontSize: 24,
+      marginBottom: 20,
+      textAlign: "center",
+    },
+    section: {
+      margin: 10,
+      padding: 10,
+      flexGrow: 1,
+    },
+    resultText: {
+      fontSize: 14,
+      lineHeight: 1.5,
+    },
+    spaceBetweenText: {
+      marginBottom: 10,
+    },
+    footer: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      textAlign: 'center',
+      padding: 10,
+      backgroundColor: '#5A189A',
+      color: 'white',
+      fontSize: 12, 
+    },
+    link: {
+      color: 'white',
+      textDecoration: 'underline',
+    },
+  });
 
-const DocPDF = ({ result }) => {
-  return (
-    <Document>
-      <Page size="A4" style={styles.page}>
-        {/* Membrete */}
-        <View style={styles.header}>
-          {/* <Image style={styles.logo} src="/path/to/your/logo.png" /> */}
-          <Text style={styles.headerText}>Copy Craft Pro</Text>
-        </View>
+  const DocPDF = ({ input, result }) => {
+    return (
+      <Document>
+        <Page size="A4" style={styles.page}>
+          
+          {/* Title Section */}
+          <View style={styles.title}>
+            <Text>{input ? input : "Sales Success"}</Text>
+          </View>
 
-        <View style={styles.separator} />
+          {/* Result Section */}
+          <View style={styles.section}>
+            <Text style={[styles.resultText, styles.spaceBetweenText]}>Take a look at the copy you generated.</Text>
+            <Text style={styles.resultText}>{result ? result : "..."}</Text>
+          </View>
 
-        {/* Título */}
-        <Text style={styles.title}>Título del PDF</Text>
+          {/* Footer */}
+          <View style={styles.footer}>
+            <Text>
+              Thank you for using Copy Craft Pro!{" "}
+              <Link src="https://copycraftpro.vercel.app" style={styles.link}>
+                Visit our web application.
+              </Link>
+            </Text>
+          </View>
+        
+        </Page>
+      </Document>
+    );
+  };
 
-        {/* Input Section */}
-        <View style={styles.section}>
-          <Text>Input</Text>
-        </View>
-
-        {/* Result Section */}
-        <View style={[styles.section, styles.resultSection]}>
-          <Text>Result</Text>
-          <Text>{result ? result : "..."}</Text>
-        </View>
-
-        {/* Pie de página */}
-        <View style={styles.footer}>
-          <Text>¡Gracias por utilizar Copy Craft Pro!</Text>
-        </View>
-      </Page>
-    </Document>
-  );
-};
-
-export default DocPDF;
+  export default DocPDF;
