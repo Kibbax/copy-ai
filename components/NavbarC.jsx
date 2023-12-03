ccc4105a55a1c0aa00b7e7990314a915ab8186db
Ezequiel Cabello <cabello.marcos.ezequiel@gmail.com>
2023-12-03 18:51:12 -0300

    Apply style folder on links

--- components/NavbarC.jsx
+++ components/NavbarC.jsx
@@ -1,16 +1,18 @@
-"use client"
-import Image from 'next/image';
-import Link from 'next/link';
-import { useState } from 'react';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 // import NavbarButton from './LogOutButton';
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoClose } from "react-icons/io5";
 import { useSession, signOut } from "next-auth/react";
+import { usePathname } from "next/navigation";
 
 function NavBar() {
   const { data: session } = useSession();
-  
+
   const [navbar, setNavbar] = useState(false);
+  const pathname = usePathname();
   return (
     <div>
       <nav className="w-full bg-black bg-opacity-60">
@@ -18,9 +20,13 @@ function NavBar() {
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
               {/* LOGO */}
-              <Link href="/" className="m-4 font-titleFont text-2xl" aria-label="Navigate to Home">
-                 COPY craft pro
-            </Link>
+              <Link
+                href="/"
+                className="m-4 font-titleFont text-2xl"
+                aria-label="Navigate to Home"
+              >
+                COPY craft pro
+              </Link>
               {/* HAMBURGER BUTTON FOR MOBILE */}
               <div className="md:hidden">
                 <button
@@ -28,7 +34,7 @@ function NavBar() {
                   onClick={() => setNavbar(!navbar)}
                 >
                   {navbar ? (
-                    <IoClose  width={30} height={30} alt="logo" />
+                    <IoClose width={30} height={30} alt="logo" />
                   ) : (
                     <RxHamburgerMenu
                       width={30}
@@ -44,41 +50,83 @@ function NavBar() {
           <div>
             <div
               className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                navbar ? 'p-12 md:p-0 block' : 'hidden'
+                navbar ? "p-12 md:p-0 block" : "hidden"
               }`}
             >
               <ul className="h-screen md:h-auto items-center justify-center md:flex ">
-              {!session?.user ? (
-                <>
-                <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                  <Link href="/auth/login" onClick={() => setNavbar(!navbar)} aria-label="Navigate to Login Page">
-                    Login
-                  </Link>
-                </li>
-                <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                <Link href="/auth/register"onClick={() => setNavbar(!navbar)}  aria-label="Navigate to Sign Up Page">
-                    Sign up
-                </Link>
-              </li>
-            </>
-          ) : (<>
-                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                  <Link href="/history" onClick={() => setNavbar(!navbar)} aria-label="Navigate to Download Page">
-                    History
-                  </Link>
-                </li>
-                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                  <Link href="/input" onClick={() => setNavbar(!navbar)} aria-label="Navigate to Input Page">
-                    Input
-                  </Link>
-                </li>
-                <li aria-label="Log Out" className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                    <button onClick={()=>signOut()}>
-                        SignOut
-                    </button>
-                </li>
-            </>
-          )}
+                {!session?.user ? (
+                  <>
+                    <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                      <Link
+                        href="/auth/login"
+                        onClick={() => setNavbar(!navbar)}
+                        aria-label="Navigate to Login Page"
+                      >
+                        <button
+                          className={`link ${
+                            pathname === "/auth/login" ? "bg-primary" : " "
+                          } hover:bg-primary font-bold py-2 px-4 rounded-tl-lg rounded-tr-lg inline-block mr-4`}
+                        >
+                          Login
+                        </button>
+                      </Link>
+                    </li>
+                    <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                      <Link
+                        href="/auth/register"
+                        onClick={() => setNavbar(!navbar)}
+                        aria-label="Navigate to Sign Up Page"
+                      >
+                        <button
+                          className={`link ${
+                            pathname === "/auth/register" ? "bg-primary" : " "
+                          } hover:bg-primary font-bold py-2 px-4 rounded-tl-lg rounded-tr-lg inline-block mr-4`}
+                        >
+                          Register
+                        </button>
+                      </Link>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                      <Link
+                        href="/history"
+                        onClick={() => setNavbar(!navbar)}
+                        aria-label="Navigate to Download Page"
+                      >
+                        <button
+                          className={`link ${
+                            pathname === "/history" ? "bg-primary" : " "
+                          } hover:bg-primary font-bold py-2 px-4 rounded-tl-lg rounded-tr-lg inline-block mr-4`}
+                        >
+                          History
+                        </button>
+                      </Link>
+                    </li>
+                    <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                      <Link
+                        href="/input"
+                        onClick={() => setNavbar(!navbar)}
+                        aria-label="Navigate to Input Page"
+                      >
+                        <button
+                          className={`link ${
+                            pathname === "/input" ? "bg-primary" : " "
+                          } hover:bg-primary font-bold py-2 px-4 rounded-tl-lg rounded-tr-lg inline-block mr-4`}
+                        >
+                          Input
+                        </button>
+                      </Link>
+                    </li>
+                    <li
+                      aria-label="Log Out"
+                      className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent"
+                    >
+                      <button onClick={() => signOut()}>SignOut</button>
+                    </li>
+                  </>
+                )}
               </ul>
             </div>
           </div>
